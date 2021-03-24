91e29107081754821e509b095a39b62d5028e5ea
Brayan Byrdsong <brayanbyrdsong@gmail.com>
2021-03-24 17:57:15 +0000

    styled link cards

--- scripts/objs/link-card.js
+++ scripts/objs/link-card.js
@@ -3,8 +3,10 @@ import i0 from '../i0.js'
 i0.obj('link-card', 
 `
     <div class="link-card">
-        <iframe width="420" height="216" i0="video" src="" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>
-        <div>
+        <div class="video">
+            <iframe width="420" height="216" i0="video" src="" title="YouTube video player" frameborder="0" allow="autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <div class="text">
             <b class="header" i0="title"></b>
             <p i0="desc"></p>
         </div>
@@ -15,6 +17,12 @@ i0.obj('link-card',
     ui.video.src = props.src
     ui.title.innerText = props.title
     ui.desc.innerText = props.desc
+
+    if(document.body.getBoundingClientRect().width < 800){
+        ui.video.width = '201px'
+        ui.video.height = '201px'
+        ui.video.margin = 'auto'
+    }
 
 })
 
